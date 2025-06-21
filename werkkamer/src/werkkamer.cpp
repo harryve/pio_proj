fa2bdd6a2986e1ef82956ab4d177f4e814168c24
Harry van Engelen <harry.v.engelen@gmail.com>
2025-06-21 16:02:51 +0200

    Led off

--- werkkamer/src/werkkamer.cpp
+++ werkkamer/src/werkkamer.cpp
@@ -4,6 +4,8 @@
 #include "network.h"
 #include "sensor.h"
 #define LED_BUILTINN D4
+#define LED_OFF HIGH
+#define LED_ON  LOW
 
 void setup()
 {
@@ -11,6 +13,7 @@ void setup()
     Serial.println("\n\nStart werkkamer sensor");
 
     pinMode(LED_BUILTINN, OUTPUT);
+    digitalWrite(LED_BUILTINN, LED_ON);
 
     NetworkInit();
     WebServerInit();
@@ -18,25 +21,17 @@ void setup()
 
     delay(1000);
     Serial.println("Setup complete");
+    digitalWrite(LED_BUILTINN, LED_OFF);
 }
 
 
 void loop()
 {
     static unsigned long previousMillis;
-    static unsigned long ledMillis;
-    static uint8_t ledState = LOW;
     unsigned long currentMillis = millis();
 
     NetworkLoop();
     WebServerLoop();
-
-    if (currentMillis - ledMillis >= 1000) {
-        ledMillis = currentMillis;
-
-        ledState = ledState == LOW ? HIGH : LOW;
-        digitalWrite(LED_BUILTINN, ledState);
-    }
 
     // Update values every 5 minutes
     if (currentMillis - previousMillis >= 1000 * 60 * 5) {
